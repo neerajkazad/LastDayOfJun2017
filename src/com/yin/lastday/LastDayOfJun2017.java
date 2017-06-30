ec371c4a09ad23350e9ce33e98c8c89abf027452
Neeaj Kumar <neeraj170291.kumar@gmail.com>
2017-06-30 11:39:59 +0530

    First Commit

---
+++ src/com/yin/lastday/LastDayOfJun2017.java
@@ -0,0 +1,7 @@
+package com.yin.lastday;
+
+public class LastDayOfJun2017 {
+	public static void main(String[] args) {
+		System.out.println("Hi This is neeraj, Today is the last day of jun 2017");
+	}
+}
